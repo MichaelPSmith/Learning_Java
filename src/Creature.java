d7d822598d4c79ca2f34299c9c5598a443573ea8
Mike Smith <mike.smith@codethink.co.uk>
2015-04-30 14:37:46 +0100

    fix bug causing creatures to spawn inside of walls.

--- src/Creature.java
+++ src/Creature.java
@@ -57,13 +57,17 @@ public abstract class Creature {
 		return result;
 	}
 	public void setRandomLocation(boolean[][] worldMap){
-		int tempX = (int) generateRandomNum(1, worldMap.length-1);
-		int tempY = (int) generateRandomNum(1, worldMap[0].length-1);
-		if (worldMap[tempX][tempY] = true)
-		{
-			this.setxCoord(tempX);
-			this.setyCoord(tempY);
-		}
+	    boolean test = false;
+		do{
+			int tempX = (int) generateRandomNum(1, worldMap.length-1);
+			int tempY = (int) generateRandomNum(1, worldMap[0].length-1);
+			if (worldMap[tempX][tempY] == true)
+			{
+				this.setxCoord(tempX);
+				this.setyCoord(tempY);
+				test = true;
+			}
+		}while(test == false);
 	}
 	public boolean isInteligent() {
 		return inteligent;

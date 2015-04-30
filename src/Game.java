5db187a6adb9e7006b131b70d718a7f2920fecf8
Mike Smith <mike.smith@codethink.co.uk>
2015-04-30 12:51:57 +0100

    gui now influces resulting map.

--- src/Game.java
+++ src/Game.java
@@ -2,17 +2,23 @@
  * the main class for the game world.
  */
 public class Game {
-	int worldWidth = 40;
-	int worldHeight = 15;
-	String[][] currentMap;
-	int denizens = (int)((Math.random()*9)+1);
+	int worldWidth = 0;
+	int worldHeight = 0;
+	int denizens = 0;
 	Creature[] worldInhabitants = new Creature[denizens];
+	String[][] currentMap;
 	/*
 	 * alter denizens to fit the number of denizens likely to be needed
 	 */
-	public Game() {
+	public Game(int worldWidthTemp, int worldHeightTemp, int denizensTemp) {
+		worldWidth = worldWidthTemp;
+		worldHeight = worldHeightTemp;
+		denizens = denizensTemp;
+		Creature[] worldInhabitants = new Creature[denizens];
+		System.out.println(worldHeight + " " + worldWidth + " " + denizens);
+		
 		currentMap = new String [worldWidth][worldHeight];
-		World current = new World(40,15);
+		World current = new World(worldWidth,worldHeight);
 		for(int y = 0; y < worldHeight; y++){
 			for(int x = 0; x < worldWidth; x++){
 				currentMap[x][y] = current.transferMap(x, y);
@@ -21,8 +27,8 @@ public class Game {
 		for(int i = 0; i < denizens; i++){
 			worldInhabitants[i] = new Human(current.getWorldMap());
 			currentMap[worldInhabitants[i].getxCoord()][worldInhabitants[i].getyCoord()] = "C";
-		}
-		
+		}		
+		System.out.println(displayMap());
 	}
 	public String displayMap(){
 		String map = "";
@@ -34,10 +40,5 @@ public class Game {
 		}
 		return map;
 	}
-	
-	
-	public static void main(String[] args) {
-		Game main = new Game();
-		System.out.println(main.displayMap());
-	}
 }
+
---
+++ src/dungeon_generater_gui.java
@@ -0,0 +1,132 @@
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+
+
+public class dungeon_generater_gui {
+
+	private JFrame frame;
+	private JSlider WorldWidthSlider;
+	private JSlider WorldHeightSlider;
+	private JLabel lblWorldPopulation;
+	private JSlider CreatureSlider;
+	private JTextArea MapDisplay;
+	private Game main;
+	public JButton btnGenerate;
+
+	public JSlider getWorldWidthSlider() {
+		return WorldWidthSlider;
+	}
+
+	public void setWorldWidthSlider(JSlider worldWidthSlider) {
+		WorldWidthSlider = worldWidthSlider;
+	}
+
+	public JSlider getWorldHeightSlider() {
+		return WorldHeightSlider;
+	}
+
+	public void setWorldHeightSlider(JSlider worldHeightSlider) {
+		WorldHeightSlider = worldHeightSlider;
+	}
+	public JButton getBtnGenerate() {
+		return btnGenerate;
+	}
+
+	public void setBtnGenerate(JButton btnGenerate) {
+		this.btnGenerate = btnGenerate;
+	}
+	public JSlider getCreatureSlider() {
+		return CreatureSlider;
+	}
+
+	public void setCreatureSlider(JSlider creatureSlider) {
+		CreatureSlider = creatureSlider;
+	}
+	public JTextArea getMapDisplay() {
+		return MapDisplay;
+	}
+
+	public void setMapDisplay(JTextArea mapDisplay) {
+		MapDisplay = mapDisplay;
+	}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					dungeon_generater_gui window = new dungeon_generater_gui();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public dungeon_generater_gui() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblWorldHeight = new JLabel("World Height");
+		lblWorldHeight.setBounds(12, 12, 112, 15);
+		frame.getContentPane().add(lblWorldHeight);
+		
+		JLabel lblWorldWidth = new JLabel("World Width");
+		lblWorldWidth.setBounds(12, 60, 112, 15);
+		frame.getContentPane().add(lblWorldWidth);
+		
+		WorldHeightSlider = new JSlider();
+		WorldHeightSlider.setBounds(12, 32, 112, 16);
+		frame.getContentPane().add(WorldHeightSlider);
+		
+		WorldWidthSlider = new JSlider();
+		WorldWidthSlider.setBounds(12, 78, 112, 16);
+		frame.getContentPane().add(WorldWidthSlider);
+		
+		btnGenerate = new JButton("Generate!");
+		btnGenerate.setBounds(12, 191, 112, 69);
+		frame.getContentPane().add(btnGenerate);
+		
+		lblWorldPopulation = new JLabel("Creatures");
+		lblWorldPopulation.setBounds(12, 106, 123, 15);
+		frame.getContentPane().add(lblWorldPopulation);
+		
+		CreatureSlider = new JSlider();
+		CreatureSlider.setBounds(12, 133, 112, 16);
+		frame.getContentPane().add(CreatureSlider);
+		
+		MapDisplay = new JTextArea();
+		MapDisplay.setBounds(142, 25, 600, 600);
+		frame.getContentPane().add(MapDisplay);
+		
+		btnGenerate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				main = new Game(WorldWidthSlider.getValue(), 
+						WorldHeightSlider.getValue(), CreatureSlider.getValue());
+	            MapDisplay.setText(main.displayMap());
+	            }
+		});
+	}
+}
